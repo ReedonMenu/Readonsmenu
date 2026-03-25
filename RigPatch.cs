ffbbb36ba8c029415310eca8727fbfacd239c97d
ReedonMenu <enzowinstanley201@gmail.com>
2026-03-25 21:03:10 +0000

    Add files via upload

---
+++ RigPatch.cs
@@ -0,0 +1,17 @@
+﻿using HarmonyLib;
+
+namespace J0kerMenuTemp.Patching
+{
+    [HarmonyPatch(typeof(VRRig), "OnDisable")]
+    internal class RigPatch
+    {
+        public static bool Prefix(VRRig __instance)
+        {
+            if (__instance == GorillaTagger.Instance.offlineVRRig)
+            {
+                return false;
+            }
+            return true;
+        }
+    }
+}
